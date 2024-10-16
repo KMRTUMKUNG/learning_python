20bd74d138b58d2e7c034e4da0818287839bba53
KMRTUMKUNG <168702912+KMRTUMKUNG@users.noreply.github.com>
2024-10-16 13:54:23 +0700

    add file git_learning

---
+++ funtion_input.py
@@ -0,0 +1,10 @@
+name = input("please Enter yourname : ")
+lname = input("please Enter yoursurkname : ")
+print("your name is = " + name)
+print("your surname is = " + lname)
+print(type(name))
+print(type(lname))
+
+result = f"{name} {lname}"
+print(result)
+#รับค่าเป็นstr ทั้งหมด
