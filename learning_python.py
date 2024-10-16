2fe6a7a92b0a6a50a1fb6995002ba2527a49e41a
KMRTUMKUNG <168702912+KMRTUMKUNG@users.noreply.github.com>
2024-10-16 13:40:00 +0700

    add file learning_python

---
+++ learning_python.py
@@ -0,0 +1,34 @@
+#string => ข้อความ ตัวอักษร ' ""
+#number => integer  float
+#boolean => true false
+print(-1)
+print(0)
+print(21)
+print(21+11)
+print("Wattana \t"+"Sewatanon")
+print("Wattana \n"+"Sewatanon")
+
+#Data_type , variable
+x = 10
+y = 3.99
+z = True
+print("ผลลัพท์ = "+str(x))
+print(y)
+print(z)
+print(type("Wattana"))
+print(type(y))
+print(type(z))
+
+#กฏการตั้งชื่อ
+#  1.ตัวแปรสามารถเป็น ตัวเลข ตัวอักษร เครื่องหมายได้
+#  2.ห้ามขึ้นต้นด้วยตัวเลข หรือเครื่องหมาย นอกจากจะมี _
+#  3.ห้ามซ้ำกับ keyword
+#  4.case sensitive ตัวพิมพ์เล็กตัวพิมพ์ใหญ่
+
+name = "A" 
+Name = "B" 
+NaMe = "C"
+
+print(name)
+print(Name)
+print(NaMe)
