02304e6233d6445f69ad3115b8625363383c0eba
KMRTUMKUNG <168702912+KMRTUMKUNG@users.noreply.github.com>
2024-10-17 15:05:15 +0700

    learningstring_part1.py

---
+++ stringep1.py
@@ -0,0 +1,12 @@
+name = " Wattana "
+
+print(len(name)) #หาจำนวนตัวอักษร
+name = name.strip()
+print(len(name))
+
+print(name.capitalize())
+print(name.replace("Wattana" , "Sewatanon")) #การเเทนที่ข้อความ
+
+x = "Watta" in name
+
+print (x)
