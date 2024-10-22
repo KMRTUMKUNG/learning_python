5a27fd7bb844ba8639f0219f6ffbb3fcd1d2826e
KMRTUMKUNG <168702912+KMRTUMKUNG@users.noreply.github.com>
2024-10-22 09:38:46 +0700

    type_coversion.py

---
+++ type_coversion.py
@@ -0,0 +1,23 @@
+x = 10
+y = 3.99
+z = "20"
+result = x+int(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#string => int
+#True =>1 False=>0
+a = False
+b = 2
+
+print(a + b)
+
+print(result)
+
+#compound Assignment
+
+t = 10
+print("ก่อน" , t)
+t+=5 #t = t+5
+t-=15 #t = t-15  
