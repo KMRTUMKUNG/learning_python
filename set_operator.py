a030bc84fd2c07583d22dc51c4bd7116493b88a0
KMRTUMKUNG <168702912+KMRTUMKUNG@users.noreply.github.com>
2024-10-23 10:37:55 +0700

    set_operator.py

---
+++ set_operator.py
@@ -0,0 +1,14 @@
+fruitA = {"banana", "mayom","apple","lemon","durian"}
+fruitB ={"apple","durian","strawberry","givi"}
+print(fruitA)
+
+fruitA.intersection_update(fruitB)
+print(fruitA)
+
+friends = {"dao","ball","mint","mind","gao"}
+x = {"dao","gao"}
+print(x.issubset(friends))
+
+#Frozenset = ข้อมูลคงที่
+
+#ค้นหาข้อมูลได้รวดเร็ว
