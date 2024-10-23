f80f5c21db29cb0d9b40ac3a0ecf3d3fb4241eaf
KMRTUMKUNG <168702912+KMRTUMKUNG@users.noreply.github.com>
2024-10-23 16:14:28 +0700

    update_gamepaoying_shup.py

--- function.py
+++ function.py
@@ -15,25 +15,30 @@
 
 import random
 words =  ("ค้อน", "กรรไกร", "กระดาษ")
+i = 0
 
-myrandom = random.choice(words)
+print("คุณมีโอกาส ตีกับบอท 3 ครั้งเท่านั้น!! เเพ้ = กาก")
+for i in range (1,4):
+    print()
+    print("รอบที่ = " , i)
+    myrandom = random.choice(words)
+    print("เกมเป่ายิ่งฉุบ!!")
+    answer = str(input("ค้อน กรรไกร กระดาษ จะออกอะไร!! ตอบ: "))
+ 
+    if answer == myrandom:
+        print("เสมอนะครับบ~~~")
 
-print("เกมเป่ายิ่งฉุบ!!")
-answer = str(input("ค้อน กรรไกร กระดาษ จะออกอะไร!! ตอบ: "))
+    elif answer == "กรรไกร"  and myrandom == "ค้อน":
+        print("คุณแพ้!! เพราะบอทออก" , myrandom)
 
-
-if answer == myrandom:
-    print("เสมอนะครับบ~~~")
-
-elif answer == "กรรไกร"  and myrandom == "ค้อน":
-    print("คุณแพ้!! เพราะบอทออก" , myrandom)
-
-elif answer == "ค้อน"  and myrandom == "กระดาษ":
-    print("คุณแพ้!! เพราะบอทออก" , myrandom)
-elif answer == "กระดาษ"  and myrandom == "กรรไกร":
-    print("คุณแพ้!! เพราะบอทออก" , myrandom)
-else :
-    print("คุรชนะ เพราะบอทออก", myrandom)
+    elif answer == "ค้อน"  and myrandom == "กระดาษ":
+        print("คุณแพ้!! เพราะบอทออก" , myrandom)
+    elif answer == "กระดาษ"  and myrandom == "กรรไกร":
+        print("คุณแพ้!! เพราะบอทออก" , myrandom)
+    else :
+        print("คุรชนะ เพราะบอทออก", myrandom)
+        break
+    
 
 print("จบโปรเเกรม!!")
 
